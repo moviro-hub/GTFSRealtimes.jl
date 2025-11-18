871964e173bf116a7b08884d6602295a3f64265c
Jonas Klasen <jrklasen@pm.me>
2025-11-18 13:27:57 +0100

    Align tests with public API names

--- src/GTFSRealtimes.jl
+++ src/GTFSRealtimes.jl
@@ -64,9 +64,10 @@ module GTFSRealtimes
 using ProtoBuf: decode, ProtoDecoder, PipeBuffer
 using Downloads
 
-# Include protobuf definitions following julia conventions
+# Include protobuf submodule with a name following julia conventions and export the module under this name
 include("transit_realtime/transit_realtime.jl")
 using .transit_realtime: transit_realtime as GTFSProtoBuf
+export GTFSProtoBuf
 
 include("types.jl")
 include("io.jl")
